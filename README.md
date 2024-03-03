2f2e6b09068c530c8ab1091bf9034261c44c9a54
francisco.carrillo <tulgot@gmail.com>
2024-03-02 22:00:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto de aprendizaje Food App
---
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.easyfood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.easyfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        navController = navHost.navController
+        binding.btmNav.setupWithNavController(navController)
+    }
+}
